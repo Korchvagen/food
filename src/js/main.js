4dcc2617b0cdd9a94b2fb6939faec2243abf37ea
Konstantin <korchikov_k@mail.ru>
2025-06-25 19:08:38 +0300

    feat: add post formData to server

--- src/js/main.js
+++ src/js/main.js
@@ -167,7 +167,7 @@ window.addEventListener('DOMContentLoaded', () => {
     render() {
       const element = document.createElement('div');
 
-      if(this.classes.length === 0) {
+      if (this.classes.length === 0) {
         this.element = 'menu__item';
         element.classList.add(this.element);
       } else {
@@ -220,4 +220,49 @@ window.addEventListener('DOMContentLoaded', () => {
     ".menu .container",
     "menu__item"
   ).render();
+
+  // Forms
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  }
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      // request.setRequestHeader('Content-type', 'multipart/form-data');
+
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      })
+    });
+  }
 });
