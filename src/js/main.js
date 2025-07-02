4157cf9339d40e1296d1d3a77a5573dff77ebc20
Konstantin <korchikov_k@mail.ru>
2025-07-02 11:56:24 +0300

    feat: add render HTML with JSON-server

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+const { data } = require("autoprefixer");
+
 window.addEventListener('DOMContentLoaded', () => {
   // Tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
@@ -174,8 +176,7 @@ window.addEventListener('DOMContentLoaded', () => {
       element.innerHTML = `
         <img src=${this.imageSrc} alt=${this.alt}>
         <h3 class="menu__item-subtitle">${this.title}</h3>
-        <div class="menu__item-descr">,
-        "menu__item"${this.text}</div>
+        <div class="menu__item-descr">${this.text}</div>
         <div class="menu__item-divider"></div>
         <div class="menu__item-price">
             <div class="menu__item-cost">Цена:</div>
@@ -187,36 +188,22 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  new Card(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container",
-    "menu__item",
-    "big"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new Card(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню "Премиум"',
-    'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    20,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-  new Card(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    16,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    return await res.json();
+  };
+
+  getResource('http://localhost:3000/menu')
+  .then(data => {
+    data.forEach(({img, altimg, title, descr, price}) => {
+      new Card(img, altimg, title, descr, price, '.menu .container').render();
+    });
+  });
 
   // Forms
   const forms = document.querySelectorAll('form');
@@ -228,10 +215,22 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -244,31 +243,20 @@ window.addEventListener('DOMContentLoaded', () => {
       form.insertAdjacentElement('afterend', statusMessage);
 
       const formData = new FormData(form);
-      const object = {};
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
-
-      fetch('server.php', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      })
-      .then(data => data.text())
-      .then(data => {
-        console.log(data);
-        showThanksModal(message.success);
-        statusMessage.remove();
-      })
-      .catch(() => {
-        showThanksModal(message.failure);
-      })
-      .finally(() => {
-        form.reset();
-      });
+      postData('http://localhost:3000/requests', json)
+        .then(data => {
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        })
+        .catch(() => {
+          showThanksModal(message.failure);
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
 
