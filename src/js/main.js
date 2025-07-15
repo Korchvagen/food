b72d9c4f6087d498bd59f06081ece6fcb6f49179
Konstantin <korchikov_k@mail.ru>
2025-07-15 14:09:49 +0300

    feat: add babel

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import timer from './modules/timer';
 import tabs from './modules/tabs';
 import modal from './modules/modal';
